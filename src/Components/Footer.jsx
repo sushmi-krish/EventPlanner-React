53793886b1c85c18dd34fc97d041f458ebd1844c
sush-krish <sushmithakrishnamoorthy@gmail.com>
2025-02-16 13:44:29 -0500

    created Footer.jsx file

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+      import React from 'react';
+     const Footer = () => {
+         return (
+             <>
+      <footer>
+      <p>&copyRight belongs to Event Planner</p>
+      </footer>
+             </>
+         )}
+
+     export default Footer
